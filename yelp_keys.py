38c72cadf8318528543fe0c01e43f09381957a7a
Kyle Hayes <khayes847@gmail.com>
2019-08-05 16:48:14 -0400

    write answers

---
+++ yelp_keys.py
@@ -0,0 +1,11 @@
+"""stores the yelp api client id and api key in a separate file for security and best practice"""
+
+def return_api_key():
+    """returns api key for yelp api"""
+    api_key = 'qAALKvYq7ugXWRnW68uyA94Li2ZLF01nQ8DNZG880qYs4LjTmWKJgVv3KP31Hg957uZhtifT-SlkfRYpqMNKCIRR9LdkvaI4-hTxSHha1KRP5jlt3HR0zOBnnmhIXXYx'
+    return api_key
+
+def return_client_id():
+    """returns client id for yelp api"""
+    client_id = '9IMARICvOmOlUfCxT-6ULg'
+    return client_id
